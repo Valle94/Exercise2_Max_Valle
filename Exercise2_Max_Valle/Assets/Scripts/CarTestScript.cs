ae7172d4518b219606f7ed72d29ee468cd00a7c5
Max Valle <55932182+Valle94@users.noreply.github.com>
2025-09-01 15:36:32 -0400

    Completed W2D1 In-Class Exercise
    Created a prefab vehicle, wall, and ground, and added textures to each component.
    
    Created a test script that prints the "Oscar Meyer Weiner" song on start.

---
+++ Exercise2_Max_Valle/Assets/Scripts/CarTestScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CarTestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Oh, I wish I were an Oscar Mayer Wiener,");
+        Debug.Log("That is what I'd truly like to be-ee-ee.");
+        Debug.Log("'Cause if I were an Oscar Mayer Wiener,");
+        Debug.Log("Everyone would be in love with me.");
+    }
+
+}
